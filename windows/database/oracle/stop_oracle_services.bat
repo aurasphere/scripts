d03c0ccb05ca9306e6ed9af4b1da9fca62c293c5
Donato Rimenti <donatohan.rimenti@gmail.com>
2016-12-07 23:30:37 +0100

    Create stop_oracle_services.bat

---
+++ windows/database/oracle/stop_oracle_services.bat
@@ -0,0 +1,7 @@
+net stop OracleDBConsoleorcl
+net stop OracleVssWriterORCL
+net stop OracleJobSchedulerORCL
+net stop OracleMTSRecoveryService
+net stop OracleOraDb11g_home1ClrAgent
+net stop OracleOraDb11g_home1TNSListener
+net stop OracleServiceORCL
